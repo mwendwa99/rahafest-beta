f0c5ca86a516f4cc14774ecc700ec87e5fc46ecc
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-03-14 16:32:41 +0300

    update modal

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Routes from "./navigation/routes";
 import { NavigationContainer } from "@react-navigation/native";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Routes />
-        <StatusBar style="auto" />
-      </View>
+      <PaperProvider>
+        <View style={styles.container}>
+          <Routes />
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
     </NavigationContainer>
   );
 }
--- screens/home/Home.js
+++ screens/home/Home.js
@@ -5,8 +5,11 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  Linking,
 } from "react-native";
 import { FontAwesome5, Entypo, Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { Portal, Modal } from "react-native-paper";
 
 import { Text, CText } from "../../components";
 
@@ -15,6 +18,10 @@ const logo = require("../../assets/logo.png");
 const blob = require("../../assets/blob.png");
 
 export default function Home({ navigation }) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleModal = () => setVisible(!visible);
+
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground
@@ -34,7 +41,7 @@ export default function Home({ navigation }) {
                 <Text variant="small" value={"Playlist"} color="#fff" />
               </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={toggleModal}>
               <ImageBackground source={blob} style={styles.blob}>
                 <Entypo name="mobile" size={40} color="#fff" />
                 <Text variant="small" value={"Socials"} color="#fff" />
@@ -43,7 +50,7 @@ export default function Home({ navigation }) {
             <TouchableOpacity>
               <ImageBackground source={blob} style={styles.blob}>
                 <Ionicons name="people-sharp" size={40} color="#fff" />
-                <Text variant="small" value={"Playlist"} color="#fff" />
+                <Text variant="small" value={"Partners"} color="#fff" />
               </ImageBackground>
             </TouchableOpacity>
           </View>
@@ -67,6 +74,52 @@ export default function Home({ navigation }) {
           </View>
         </View>
       </ImageBackground>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={toggleModal}
+          contentContainerStyle={styles.modalStyle}
+        >
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-evenly" }}
+          >
+            <FontAwesome5
+              name="tiktok"
+              size={38}
+              color="#fff"
+              style={{ marginHorizontal: 10 }}
+              onPress={() =>
+                Linking.openURL("https://www.tiktok.com/@rahafest/")
+              }
+            />
+            <Entypo
+              name="twitter"
+              size={40}
+              color="#4267B2"
+              style={{ marginHorizontal: 10 }}
+              onPress={() => Linking.openURL("https://twitter.com/raha_fest")}
+            />
+            <Entypo
+              name="instagram"
+              size={40}
+              color="#C13584"
+              style={{ marginHorizontal: 10 }}
+              onPress={() =>
+                Linking.openURL("https://www.instagram.com/rahafest/")
+              }
+            />
+            <Entypo
+              name="youtube"
+              size={40}
+              color="#FF0000"
+              style={{ marginHorizontal: 10 }}
+              onPress={() =>
+                Linking.openURL("https://www.youtube.com/@rahafest")
+              }
+            />
+          </View>
+        </Modal>
+      </Portal>
     </SafeAreaView>
   );
 }
@@ -110,5 +163,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     margin: 10,
+  },
+  modalStyle: {
+    padding: 20,
+    borderRadius: 10,
+    marginHorizontal: 10,
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
 });
