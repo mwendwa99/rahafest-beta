5f1f6332bbc3bf06be4033549488beb6e071bb85
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-03-16 19:54:39 +0300

    update auth screens

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Input } from "./Input";
 export { default as VideoPlayer } from "./VideoPlayer";
 export { default as Avatar } from "./Avatar";
 export { default as ListItem } from "./ListItem";
+export { default as Button } from "./Button";
