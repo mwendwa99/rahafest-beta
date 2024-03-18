5baa22cb1141be31d77f97dd5fe5845dea325fdf
kunnoh <alvindereba@gmail.com>
2024-03-18 09:13:08 +0300

    added menu

--- screens/menu/Menu.js
+++ screens/menu/Menu.js
@@ -1,26 +1,52 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 import { Text } from "../../components";
 import { StatusBar } from "expo-status-bar";
+import { TabView, SceneMap } from "react-native-tab-view";
+import Drinks from "./drinks";
+import food from "./food";
+import { useState } from "react";
+
+const scene = SceneMap({
+  drinks: Drinks,
+  food: food
+});
 
 export default function Menu({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text value="Menu ui here..." variant="subtitle" />
-      <StatusBar style="light" />
-    </View>
-  );
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(0);
+  const [routes, setRoutes] = useState([
+    { key: "food", title: "Food"},
+    { key: "drinks", title: "Drinks" },
+  ]);
+
+  const handleIndexChange = (selectedIndex) => {
+    if (selectedIndex >= 0 && selectedIndex < routes.length) {
+      setIndex(selectedIndex);
+    } else {
+      console.error("Invalid index provided:", selectedIndex);
+    }
+  };
+
+
+  return(
+    <TabView 
+      style={styles.tabview}
+      navigationState={{ index, routes }}
+      renderScene={scene}
+      onIndexChange={handleIndexChange}
+      initialLayout={{ width: layout.width }}
+  />
+);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#212529",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
   },
-  row: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
+  tabview: {
+      flex: 1,
+  }
 });
+
