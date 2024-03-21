136704f38726dd1342f0985920f33ea483677cf2
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-03-21 14:55:32 +0300

    update delete function

--- env.js
+++ env.js
@@ -4,5 +4,6 @@ export const prod = {
   mobile: "https://sapient.stackthon.com/api",
   image: "https://api.rahafest.com/",
   chat: "https://rahachat.stackthon.com/api",
+  delete: "https://rahachat.stackthon.com/api",
   // chat: "https://chat.rahafest.com/api",
 };
