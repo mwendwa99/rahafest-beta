639c4667a763f1cd46b1dd06101b9d98ce86e607
kunnoh <alvindereba@gmail.com>
2024-03-25 09:49:45 +0300

    ready for build

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 export const prod = {
   production: true,
-  web: "https://chat.rahafest.com/api",
+  web: "https://api.rahafest.com/api",
+  mobile: "https://sapient.stackthon.com/api",
   image: "https://api.rahafest.com/",
   chat: "https://rahachat.stackthon.com/api",
   delete: "https://rahachat.stackthon.com/api",
