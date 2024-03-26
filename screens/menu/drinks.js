70bd4ebe0de12d37501e0c613ec752f5a1303798
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-03-26 11:21:42 +0300

    update static menu

--- screens/menu/drinks.js
+++ screens/menu/drinks.js
@@ -9,6 +9,189 @@ import {
   ScrollView,
 } from "react-native";
 
+const local = [
+  {
+  name: "Tusker/Tusker malt/Tusker Lite",
+  price: "350"
+  },
+  {
+    name: "White Cap",
+    price: "350"
+  },
+  {
+    name: "Guiness",
+    price: "350"
+  },
+  {
+    name: "Manyatta",
+    price: "400"
+  }];
+
+const inter = [{
+    name: "Heineken 0.0",
+    price: "350"
+  },
+  {
+    name: "Heineken",
+    price: "350"
+  },
+  {
+    name: "Desperados",
+    price: "400"
+  },
+  {
+    name: "Savanna",
+    price: "400"
+  }];
+
+const whisky = [{
+    name: "Jameson Original 700ml",
+    price: "4000"
+  },
+  {
+    name: "Jameson Black barrel 700ml",
+    price: "6500"
+  },
+  {
+    name: "Jameson Original 350ml",
+    price: "2800"
+  }
+];
+
+const cognac = [
+  {
+    name: "Martel VS 700ml",
+    price: "7000"
+  },
+  {
+    name: "Martell Swift",
+    price: "12000"
+  },
+  {
+    name: "Martell Cordon",
+    price: "20000"
+  },
+  {
+    name: "Martell XO",
+    price: "45000"
+  }
+];
+
+const shots = [
+  {
+    name: "Absolut Vodka 700ml",
+    price: "300"
+  },
+  {
+    name: "Beefeater Gin 700ml",
+    price: "300"
+  },
+  {
+    name: "Beefeater Pink Gin",
+    price: "300"
+  },
+  {
+    name: "Olmecca 750ml ",
+    price: "300"
+  },
+  {
+    name: "Olmecca Gold 750 ml",
+    price: "300"
+  },
+  {
+    name: "Olmecca Fusion Choc 75ml",
+    price: "300"
+  },
+  {
+    name: "ameson Original 750ml",
+    price: "300"
+  },
+  {
+    name: "Jameson Black Barrell 750ml",
+    price: "350"
+  },
+  {
+    name: "Martell Vs 700ml ",
+    price: "400"
+  }
+];
+
+const champagne = [
+  {
+    name: "Mumm Champagne Cordon Rough Brut",
+    price: "12500"
+  },
+  {
+    name: "Mumm Champagne Cordon Rose",
+    price: "12500"
+  },
+  {
+    name: "Mumm Champagne Demi-Sec",
+    price: "12500"
+  },
+];
+
+const sparkling = [
+  {
+    name: "Luc Belaire Rose",
+    price: "8500"
+  },
+  {
+    name: "Luc Belaire Gold",
+    price: "8500"
+  },
+  {
+    name: "Luc Belaire Rose Fantome",
+    price: "9500"
+  },
+  {
+    name: "Luc Belaire Luxe",
+    price: "8500"
+  },
+  {
+    name: "Luc Belaire Rose 150cl",
+    price: "14000"
+  }
+];
+
+const softdrink = [
+  {
+    name: "Highlands water",
+    price: "150"
+  },
+  {
+    name: "Soda",
+    price: "200"
+  },
+  {
+    name: "Redbull",
+    price: "350"
+  },
+];
+
+const cocktails = [
+  {
+    name: "Jameson Original Cocktails",
+    price: "400"
+  },
+  {
+    name: "Jameson Black Barrel Cocktails",
+    price: "500"
+  },
+  {
+    name: "Jameson & Redbull ",
+    price: "700"
+  },
+  {
+    name: "Martell Vs Cocktail",
+    price: "600"
+  },
+  {
+    name: "Martell Swift Cocktail",
+    price: "750"
+  }
+];
+
 const Drinks = () => {
   return (
     <ImageBackground
@@ -17,62 +200,119 @@ const Drinks = () => {
       <SafeAreaView style={styles.safeArea}>
         <ScrollView style={styles.scrollView}>
           <View style={styles.categoryWrapper}>
-            <Text style={styles.category}>Cocktails</Text>
+            <Text style={styles.category}>Local Beers</Text>
             <View style={styles.subCategory}>
-              <Text style={styles.subCategoryTxt}>Original Vanguard</Text>
-              <View>
-                <Text style={styles.drink}>- Jameson</Text>
-                <Text style={styles.drink}>- Tonic water</Text>
-                <Text style={styles.drink}>- Elderflower Cordial</Text>
+              {local.map((item, index) => (
+              <View key={index}>
+                <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
               </View>
-            </View>
-            <View style={styles.subCategory}>
-              <Text style={styles.subCategoryTxt}>Raha Cooler</Text>
-              <View>
-                <Text style={styles.drink}>- Glenlivet Founders</Text>
-                <Text style={styles.drink}>- Ginger Ale</Text>
-              </View>
-            </View>
-            <View style={styles.subCategory}>
-              <Text style={styles.subCategoryTxt}>Martel Old Fashioned</Text>
-              <View>
-                <Text style={styles.drink}>- Martel VS</Text>
-                <Text style={styles.drink}>- Angostura</Text>
-                <Text style={styles.drink}>- Granulated Sugar</Text>
-                <Text style={styles.drink}>- Orange Twist</Text>
-              </View>
-            </View>
-            <View style={styles.subCategory}>
-              <Text style={styles.subCategoryTxt}>The Celeb</Text>
-              <View>
-                <Text style={styles.drink}>- Chivas XV</Text>
-                <Text style={styles.drink}>- Grape Fruit Soda</Text>
-              </View>
-            </View>
-            <View style={styles.subCategory}>
-              <Text style={styles.subCategoryTxt}>Rum and Coke</Text>
-              <View>
-                <Text style={styles.drink}>- Captain Morgan</Text>
-                <Text style={styles.drink}>- Coke</Text>
-              </View>
+              ))}
             </View>
           </View>
 
           <View style={styles.categoryWrapper}>
-            <Text style={styles.category}>local beers</Text>
+            <Text style={styles.category}>International Beers & Ciders</Text>
             <View style={styles.subCategory}>
-              <Text style={styles.beer}>Tusker ......... 300</Text>
-              <Text style={styles.beer}>White Cup ...... 300</Text>
-              <Text style={styles.beer}>Balozi ......... 300</Text>
+              {inter.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Whisky</Text>
+            <View style={styles.subCategory}>
+              {whisky.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Cognac</Text>
+            <View style={styles.subCategory}>
+              {shots.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Shots</Text>
+            <View style={styles.subCategory}>
+              {shots.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Champagne</Text>
+            <View style={styles.subCategory}>
+              {champagne.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Sparkling Wine</Text>
+            <View style={styles.subCategory}>
+              {sparkling.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.categoryWrapper}>
+            <Text style={styles.category}>Soft Drinks</Text>
+            <View style={styles.subCategory}>
+              {softdrink.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
             </View>
           </View>
 
           <View style={[styles.categoryWrapper, styles.beerCider]}>
-            <Text style={styles.category}>other beers & ciders</Text>
+            <Text style={styles.category}>Cocktails</Text>
             <View style={styles.subCategory}>
-              <Text style={styles.beer}>Heineken ....... 400</Text>
-              <Text style={styles.beer}>Desperado ...... 400</Text>
-              <Text style={styles.beer}>Savanna ........ 400</Text>
+              {cocktails.map((item, index) => (
+              <View key={index}>
+              <Text style={styles.drink}>
+                  {item.name} ...... {item.price}
+                </Text>
+              </View>
+              ))}
             </View>
           </View>
         </ScrollView>
@@ -136,3 +376,142 @@ const styles = StyleSheet.create({
     zIndex: -1,
   },
 });
+
+//import React from "react";
+//import {
+//  SafeAreaView,
+//  StyleSheet,
+//  Text,
+//  View,
+//  ImageBackground,
+//  Image,
+//  ScrollView,
+//} from "react-native";
+//
+//const Drinks = () => {
+//  return (
+//    <ImageBackground
+//      source={require("../../assets/images/BarMenu.png")}
+//      style={{ flex: 1, position: "relative", marginTop: 0, height: "30%" }}>
+//      <SafeAreaView style={styles.safeArea}>
+//        <ScrollView style={styles.scrollView}>
+//          <View style={styles.categoryWrapper}>
+//            <Text style={styles.category}>Cocktails</Text>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.subCategoryTxt}>Original Vanguard</Text>
+//              <View>
+//                <Text style={styles.drink}>- Jameson</Text>
+//                <Text style={styles.drink}>- Tonic water</Text>
+//                <Text style={styles.drink}>- Elderflower Cordial</Text>
+//              </View>
+//            </View>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.subCategoryTxt}>Raha Cooler</Text>
+//              <View>
+//                <Text style={styles.drink}>- Glenlivet Founders</Text>
+//                <Text style={styles.drink}>- Ginger Ale</Text>
+//              </View>
+//            </View>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.subCategoryTxt}>Martel Old Fashioned</Text>
+//              <View>
+//                <Text style={styles.drink}>- Martel VS</Text>
+//                <Text style={styles.drink}>- Angostura</Text>
+//                <Text style={styles.drink}>- Granulated Sugar</Text>
+//                <Text style={styles.drink}>- Orange Twist</Text>
+//              </View>
+//            </View>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.subCategoryTxt}>The Celeb</Text>
+//              <View>
+//                <Text style={styles.drink}>- Chivas XV</Text>
+//                <Text style={styles.drink}>- Grape Fruit Soda</Text>
+//              </View>
+//            </View>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.subCategoryTxt}>Rum and Coke</Text>
+//              <View>
+//                <Text style={styles.drink}>- Captain Morgan</Text>
+//                <Text style={styles.drink}>- Coke</Text>
+//              </View>
+//            </View>
+//          </View>
+//
+//          <View style={styles.categoryWrapper}>
+//            <Text style={styles.category}>local beers</Text>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.beer}>Tusker ......... 300</Text>
+//              <Text style={styles.beer}>White Cup ...... 300</Text>
+//              <Text style={styles.beer}>Balozi ......... 300</Text>
+//            </View>
+//          </View>
+//
+//          <View style={[styles.categoryWrapper, styles.beerCider]}>
+//            <Text style={styles.category}>other beers & ciders</Text>
+//            <View style={styles.subCategory}>
+//              <Text style={styles.beer}>Heineken ....... 400</Text>
+//              <Text style={styles.beer}>Desperado ...... 400</Text>
+//              <Text style={styles.beer}>Savanna ........ 400</Text>
+//            </View>
+//          </View>
+//        </ScrollView>
+//        <Image
+//          style={styles.bottomImg}
+//          source={require("../../assets/images/barbottom.png")}
+//        />
+//      </SafeAreaView>
+//    </ImageBackground>
+//  );
+//};
+//
+//export default Drinks;
+//
+//const styles = StyleSheet.create({
+//  safeArea: {
+//    flex: 1,
+//  },
+//  container: {
+//    alignItems: "center",
+//    flex: 1,
+//    justifyContent: "center",
+//  },
+//  scrollView: {
+//    flex: 1,
+//    marginTop: "58%",
+//    padding: 40,
+//    paddingBottom: 100,
+//  },
+//  categoryWrapper: {
+//    marginBottom: 30,
+//  },
+//  beerCider: {
+//    marginBottom: "70%",
+//  },
+//  category: {
+//    textTransform: "uppercase",
+//    fontWeight: "bold",
+//    fontSize: 24,
+//  },
+//  subCategory: {
+//    left: 15,
+//    marginBottom: 25,
+//  },
+//  subCategoryTxt: {
+//    fontSize: 20,
+//  },
+//  drink: {
+//    left: 15,
+//    fontSize: 16,
+//  },
+//  beer: {
+//    fontSize: 20,
+//  },
+//  bottomImg: {
+//    position: "absolute",
+//    flex: 1,
+//    bottom: -40,
+//    width: "100%",
+//    objectFit: "contain",
+//    zIndex: -1,
+//  },
+//});
--- screens/menu/food.js
+++ screens/menu/food.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Image,
   ImageBackground,
@@ -7,101 +7,162 @@ import {
   StyleSheet,
   Text,
   View,
-  RefreshControl,
 } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { getMenu } from "../../redux/menu/menuActions";
-import { StatusBar } from "expo-status-bar";
+import { useSelector } from "react-redux";
 
-const splash = require("../../assets/splash.png");
-const background = require("../../assets/images/istockphoto-518037033-170667a-1.webp");
+const homegrown = [
+  {
+    name: "Smokies + Fries",
+    price: "350",
+  },
+  {
+    name: "Mayai Pasua",
+    price: "50",
+  },
+  {
+    name: "Chicken Mshikaki + Fries",
+    price: "500",
+  },
+  {
+    name: "Beef Mshikaki + Fries",
+    price: "500",
+  },
+  {
+    name: "Plain Fries",
+    price: "250",
+  },
+  {
+    name: "Shawarma",
+    price: "500",
+  },
+];
+
+const chomaZone = [
+  {
+    name: "Goat Choma (1kg; 1/2kg; 1/4kg) + Fries",
+    price: "1400; 750; 400",
+  },
+  {
+    name: "Beef Choma (1kg; 1/2kg; 1/4kg) + Fries",
+    price: "1300; 650; 300",
+  },
+  {
+    name: "Chicken Choma (Full; Half; Quater) + Fries",
+    price: "1500; 750; 400",
+  },
+  {
+    name: "Choma Sausage(2 pieces)",
+    price: "500",
+  },
+];
+
+const continental = [
+  {
+    name: "Wings + Fries",
+    price: "600",
+  },
+  {
+    name: "Hot Dogs + Fries",
+    price: "700",
+  },
+  {
+    name: "Beef Burger",
+    price: "800",
+  },
+];
+
 const vanguardLogo = require("../../assets/images/VANGUARDLOGOFINAL.jpeg");
-
+const Bg = require("../../assets/images/istockphoto-518037033-170667a-1.webp");
 const Food = () => {
-  const dispatch = useDispatch();
-  const { menu, loading } = useSelector((state) => state.menu);
-  const [refreshing, setRefreshing] = useState(false);
+  const { menu } = useSelector((state) => state.menu);
+  // console.log(menu);
+  const itemsBySubcategory = {};
+  menu.forEach(item => {
+      const subcategoryName = item.subcategory.name;
+      if (!itemsBySubcategory[subcategoryName]) {
+          itemsBySubcategory[subcategoryName] = [];
+      }
+      itemsBySubcategory[subcategoryName].push(item);
+  });
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    dispatch(getMenu());
-    setRefreshing(false);
-  };
-
-  useEffect(() => {
-    dispatch(getMenu());
-  }, [dispatch]);
-
-  // Group menu items by subcategory
-  const groupedMenu = menu.reduce((acc, item) => {
-    const subcategory = item.subcategory.name;
-    if (!acc[subcategory]) {
-      acc[subcategory] = [];
-    }
-    acc[subcategory].push(item);
-    return acc;
-  }, {});
-
-  if (loading) {
-    return (
-      <ImageBackground
-        source={splash}
-        style={{ flex: 1, justifyContent: "center" }}
-      >
-        <StatusBar style="light" />
-      </ImageBackground>
-    );
-  }
+  // Log the items grouped by subcategory
+  Object.keys(itemsBySubcategory).forEach(subcategory => {
+      console.log(`Subcategory: ${subcategory}`);
+      itemsBySubcategory[subcategory].forEach(item => {
+          console.log(`- ${item.name}`);
+      });
+  });
 
   return (
     <ImageBackground
-      source={background}
-      style={{ flex: 1, position: "relative", marginTop: 0, height: "30%" }}
-    >
+      source={Bg}
+      style={{ flex: 1, position: "relative", marginTop: 0, height: "30%" }}>
       <SafeAreaView style={styles.container}>
         <View style={styles.imgWrapper}>
-          <Image style={styles.img} source={vanguardLogo} />
+          <Image
+            style={styles.img}
+            source={vanguardLogo}
+          />
         </View>
         <View style={styles.menuContainer}>
           <View style={styles.menuWrapper}>
             <Text style={styles.menu}>MENU</Text>
           </View>
-          <ScrollView
-            style={{ flex: 1 }}
-            refreshControl={
-              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }
-          >
+          <ScrollView style={{ flex: 1 }}>
             <View style={styles.food}>
-              {Object.entries(groupedMenu).map(([subcategory, items]) => (
-                <View key={subcategory} style={{ marginTop: 30 }}>
-                  <Text style={styles.category}>{subcategory}</Text>
-                  {items.map((item, index) => (
-                    <View
-                      key={index}
-                      style={{
-                        borderBottomWidth: 1,
-                        borderColor: "grey",
-                        borderStyle: "dashed",
-                        paddingVertical: 10,
-                      }}
-                    >
-                      <Text key={index}>
-                        {item.name} - {item.price}
-                      </Text>
-                      <Text>{item.description}</Text>
-                    </View>
-                  ))}
-                </View>
-              ))}
+              {/* <View> */}
+              <View>
+                <Text style={styles.category}>Homegrown</Text>
+                {homegrown.map((item, index) => (
+                  <Text key={index}>
+                    {item.name} ...... {item.price}
+                  </Text>
+                ))}
+              </View>
+
+              <View style={{ marginTop: 30 }}>
+                <Text style={styles.category}>chomaZone</Text>
+                <Text>Goat Choma(+Fries)</Text>
+                <Text style={styles.quantity}> 1kg ...... 1400</Text>
+                <Text style={styles.quantity}> 1/2kg ...... 750</Text>
+                <Text style={styles.quantity}> 1/4kg ...... 400</Text>
+
+                <Text>Beef Choma(+Fries)</Text>
+                <Text style={styles.quantity}> 1kg ...... 1300</Text>
+                <Text style={styles.quantity}> 1/2kg ...... 650</Text>
+                <Text style={styles.quantity}> 1/4kg ...... 300</Text>
+
+                <Text>Chicken Choma(+Fries)</Text>
+                <Text style={styles.quantity}> 1kg ...... 1500</Text>
+                <Text style={styles.quantity}> 1/2kg ...... 750</Text>
+                <Text style={styles.quantity}> 1/4kg ...... 400</Text>
+
+                <Text>Choma Sausage(2 pieces) ...... 500</Text>
+
+                {/* {chomaZone.map((item, index) => (
+                  <Text key={index}>
+                    {item.name} ...... {item.price}
+                  </Text>
+                ))} */}
+              </View>
+
+              <View style={{ marginTop: 30, marginBottom: 30 }}>
+                <Text style={styles.category}>continental</Text>
+                {continental.map((item, index) => (
+                  <Text key={index}>
+                    {item.name} ...... {item.price}
+                  </Text>
+                ))}
+              </View>
+              {/* </View> */}
             </View>
           </ScrollView>
         </View>
       </SafeAreaView>
-      <StatusBar style="light" />
     </ImageBackground>
   );
 };
+
 export default Food;
 
 const styles = StyleSheet.create({
@@ -150,3 +211,157 @@ const styles = StyleSheet.create({
     left: 5,
   },
 });
+
+
+//import React, { useEffect, useState } from "react";
+//import {
+//  Image,
+//  ImageBackground,
+//  SafeAreaView,
+//  ScrollView,
+//  StyleSheet,
+//  Text,
+//  View,
+//  RefreshControl,
+//} from "react-native";
+//import { useDispatch, useSelector } from "react-redux";
+//import { getMenu } from "../../redux/menu/menuActions";
+//import { StatusBar } from "expo-status-bar";
+//
+//const splash = require("../../assets/splash.png");
+//const background = require("../../assets/images/istockphoto-518037033-170667a-1.webp");
+//const vanguardLogo = require("../../assets/images/VANGUARDLOGOFINAL.jpeg");
+//
+//const Food = () => {
+//  const dispatch = useDispatch();
+//  const { menu, loading } = useSelector((state) => state.menu);
+//  const [refreshing, setRefreshing] = useState(false);
+//
+//  const onRefresh = () => {
+//    setRefreshing(true);
+//    dispatch(getMenu());
+//    setRefreshing(false);
+//  };
+//
+//  useEffect(() => {
+//    dispatch(getMenu());
+//  }, [dispatch]);
+//
+//  // Group menu items by subcategory
+//  const groupedMenu = menu.reduce((acc, item) => {
+//    const subcategory = item.subcategory.name;
+//    if (!acc[subcategory]) {
+//      acc[subcategory] = [];
+//    }
+//    acc[subcategory].push(item);
+//    return acc;
+//  }, {});
+//
+//  if (loading) {
+//    return (
+//      <ImageBackground
+//        source={splash}
+//        style={{ flex: 1, justifyContent: "center" }}
+//      >
+//        <StatusBar style="light" />
+//      </ImageBackground>
+//    );
+//  }
+//
+//  return (
+//    <ImageBackground
+//      source={background}
+//      style={{ flex: 1, position: "relative", marginTop: 0, height: "30%" }}
+//    >
+//      <SafeAreaView style={styles.container}>
+//        <View style={styles.imgWrapper}>
+//          <Image style={styles.img} source={vanguardLogo} />
+//        </View>
+//        <View style={styles.menuContainer}>
+//          <View style={styles.menuWrapper}>
+//            <Text style={styles.menu}>MENU</Text>
+//          </View>
+//          <ScrollView
+//            style={{ flex: 1 }}
+//            refreshControl={
+//              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+//            }
+//          >
+//            <View style={styles.food}>
+//              {Object.entries(groupedMenu).map(([subcategory, items]) => (
+//                <View key={subcategory} style={{ marginTop: 30 }}>
+//                  <Text style={styles.category}>{subcategory}</Text>
+//                  {items.map((item, index) => (
+//                    <View
+//                      key={index}
+//                      style={{
+//                        borderBottomWidth: 1,
+//                        borderColor: "grey",
+//                        borderStyle: "dashed",
+//                        paddingVertical: 10,
+//                      }}
+//                    >
+//                      <Text key={index}>
+//                        {item.name} - {item.price}
+//                      </Text>
+//                      <Text>{item.description}</Text>
+//                    </View>
+//                  ))}
+//                </View>
+//              ))}
+//            </View>
+//          </ScrollView>
+//        </View>
+//      </SafeAreaView>
+//      <StatusBar style="light" />
+//    </ImageBackground>
+//  );
+//};
+//export default Food;
+//
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    flexDirection: "column",
+//  },
+//  imgWrapper: {
+//    marginLeft: 60,
+//    width: "30%",
+//    height: "32%",
+//    backgroundColor: "#bc4a0d",
+//  },
+//  menuContainer: {
+//    position: "relative",
+//    display: "flex",
+//    flexDirection: "row",
+//    flex: 1,
+//  },
+//  food: {
+//    width: "100%",
+//    zIndex: 99,
+//  },
+//  category: {
+//    fontWeight: "bold",
+//    fontSize: 24,
+//    textTransform: "uppercase",
+//  },
+//  img: {
+//    flex: 1,
+//    width: "100%",
+//    objectFit: "contain",
+//  },
+//  menuWrapper: {
+//    width: "30%",
+//    alignItems: "center",
+//    justifyContent: "center",
+//  },
+//  menu: {
+//    fontSize: 40,
+//    fontWeight: "bold",
+//    transform: [{ rotate: "270deg" }],
+//    textAlign: "center",
+//  },
+//  quantity: {
+//    left: 5,
+//  },
+//});
