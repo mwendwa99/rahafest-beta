99e64271d9cedcf2db0ba0ea5317d18803f95afc
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-05-23 16:03:00 +0300

    update events loading

--- App.js
+++ App.js
@@ -9,11 +9,24 @@ import { RootSiblingParent } from "react-native-root-siblings";
 
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import splash from "./assets/splash.png";
+import { ImageBackground } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate
+        persistor={persistor}
+        loading={
+          <ImageBackground
+            source={splash}
+            style={{ flex: 1, justifyContent: "center", height: "100vh" }}
+          >
+            <StatusBar style="light" />
+          </ImageBackground>
+        }
+      >
         <NavigationContainer>
           <PaperProvider>
             <SafeAreaProvider>
--- screens/home/Events.js
+++ screens/home/Events.js
@@ -2,53 +2,55 @@ import { StyleSheet, ImageBackground } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { WebView } from "react-native-webview";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const splash = require("../../assets/splash.png");
 
 export default function Checkout() {
   const [loading, setLoading] = useState(true);
 
-  const handleLoadStart = () => {
-    setLoading(true);
-  };
-
   const handleLoadEnd = () => {
     setLoading(false);
   };
 
+  const handleLoadError = () => {
+    setLoading(false); // You might want to handle errors differently
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      {loading && (
+      {loading ? (
         <ImageBackground
           source={splash}
           style={{ flex: 1, justifyContent: "center", height: "100vh" }}
         >
           <StatusBar style="light" />
         </ImageBackground>
+      ) : (
+        // Display the iframe after loading is complete
+        <WebView
+          onLoad={handleLoadEnd}
+          onError={handleLoadError}
+          source={{
+            html: `
+              <html>
+                <head>
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                </head>
+                <body style="margin: 0;">
+                  <iframe
+                    style="border-radius:12px; object-fit:cover; width: 100%; height: 100%;"
+                    src="https://tickoh.netlify.app/raha-events"
+                    frameBorder="0"
+                    allow="fullscreen"
+                    loading="lazy"
+                  ></iframe>
+                </body>
+              </html>
+            `,
+          }}
+        />
       )}
-      <WebView
-        onLoadStart={handleLoadStart}
-        onLoadEnd={handleLoadEnd}
-        source={{
-          html: `
-            <html>
-              <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-              </head>
-              <body style="margin: 0;">
-                <iframe
-                  style="border-radius:12px; object-fit:cover; width: 100%; height: 100%;"
-                  src="https://tickoh.netlify.app/raha-events"
-                  frameBorder="0"
-                  allow="fullscreen"
-                  loading="lazy"
-                ></iframe>
-              </body>
-            </html>
-          `,
-        }}
-      />
       <StatusBar style="light" />
     </SafeAreaView>
   );
