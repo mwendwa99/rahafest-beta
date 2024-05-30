f851f309ee3a63a3ca8cbc401a50f96ec924204c
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-05-30 10:18:47 +0300

    cleanup playlist, remove unnecessary assets

--- screens/home/Playlist.js
+++ screens/home/Playlist.js
@@ -1,70 +1,8 @@
-import {
-  ActivityIndicator,
-  View,
-  StyleSheet,
-  ScrollView,
-  FlatList,
-  ImageBackground,
-} from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { WebView } from "react-native-webview";
 import { useState } from "react";
-
-import { VideoPlayer, Text } from "../../components";
-
-const videoIds = [
-  {
-    id: 1,
-    videoId: "SbgKpHi-Cao",
-    artist: "Davido",
-  },
-  {
-    id: 2,
-    videoId: "NPCC02SaJVg",
-    artist: "King Promise",
-  },
-  {
-    id: 4,
-    videoId: "yuGwHO-DPOs",
-    artist: "Musa Keys",
-  },
-  {
-    id: 3,
-    videoId: "XVkkuTwaXgY",
-    artist: "Ya Levis",
-  },
-  {
-    id: 10,
-    videoId: "L1nYNjZX3K4",
-    artist: "Jb Mpiana",
-  },
-  {
-    id: 5,
-    videoId: "dU30FLgld50",
-    artist: "Otile Brown",
-  },
-  {
-    id: 6,
-    videoId: "7cV9DpE7yy8",
-    artist: "Bensoul",
-  },
-  {
-    id: 7,
-    videoId: "xHN_G3jdbdA",
-    artist: "Nviiri the Storyteller",
-  },
-  {
-    id: 8,
-    videoId: "6Ea-3ooL9Ps",
-    artist: "Femi One",
-  },
-  {
-    id: 9,
-    videoId: "IMw9Q8TM37E",
-    artist: "H_art the band",
-  },
-];
 
 const splash = require("../../assets/splash.png");
 
@@ -93,25 +31,22 @@ export default function Playlist() {
         onLoadStart={handleLoadStart}
         onLoadEnd={handleLoadEnd}
         style={{
+          flex: 1,
           backgroundColor: "transparent",
-          height: "100%",
-          padding: 0,
-          objectFit: "fill",
         }}
         source={{
-          html: '<iframe style="border-radius:12px; object-fit:fill;" src="https://open.spotify.com/embed/playlist/1GW2VyfbDFFc0QtdHi5TGr?utm_source=generator" width="100%" height="100%" frameBorder="0" allow="fullscreen" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+          html: `
+          <html>
+                <head>
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                </head>
+                <body style="margin: 0;">
+          <iframe style="border-radius:12px; object-fit:fill;" src="https://open.spotify.com/embed/playlist/1GW2VyfbDFFc0QtdHi5TGr?utm_source=generator" width="100%" height="100%" frameBorder="0" allow="fullscreen" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"
+          ></iframe>
+           </body>
+              </html>`,
         }}
       />
-      {/* <FlatList
-        data={videoIds}
-        renderItem={({ item }) => (
-          <View style={{ marginBottom: 10 }}>
-            <Text value={item.artist} color="#fafafa" variant="subtitle" />
-            <VideoPlayer video={item.videoId} />
-          </View>
-        )}
-        keyExtractor={(item) => item.id}
-      /> */}
       <StatusBar style="light" />
     </SafeAreaView>
   );
