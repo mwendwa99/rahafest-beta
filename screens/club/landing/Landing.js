28c013c8d81a31aafdbfca7d2cbd8ecc3dcb0510
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-07-15 11:29:47 +0300

    merged main

--- screens/club/landing/Landing.js
+++ screens/club/landing/Landing.js
@@ -6,33 +6,16 @@ const navigationItems = [
   { id: "2", icon: "users", title: "Friends", link: "Friends" },
   { id: "3", icon: "tags", title: "Merchandise", link: "Merchandise" },
   { id: "4", icon: "camera-retro", title: "Media", link: "Media" },
-<<<<<<< HEAD
-  { id: "5", icon: "ticket", title: "Event Deals", link: "EventDeals" },
-  { id: "6", icon: "newspaper-o", title: "News", link: "News" },
-];
-
-const userName = "Brian Mwendwa";
-=======
   // { id: "5", icon: "ticket", title: "Event Deals", link: "Checkout" },
   // { id: "6", icon: "newspaper-o", title: "News", link: "News" },
 ];
 
 const userName = "Raha Fan";
->>>>>>> 744c17ce36b8f699374430de4b88f6e795ce72ea
 const memberDate = "2024";
 const rahaClubDescription =
   "Welcome to Raha Club, your exclusive RahaFest companion";
 
 export default function Landing({ navigation }) {
-<<<<<<< HEAD
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Hi {userName}</Text>
-        <Text style={{ fontSize: 12, color: "gray" }}>
-          Member since {memberDate}
-        </Text>
-=======
   const handlePress = (link) => {
     if (link === "Friends" || link === "Media") {
       Alert.alert(
@@ -51,7 +34,6 @@ export default function Landing({ navigation }) {
           Hello there {userName}
         </Text>
 
->>>>>>> 744c17ce36b8f699374430de4b88f6e795ce72ea
         <Text style={{ fontSize: 16, marginTop: 16 }}>
           {rahaClubDescription}
         </Text>
