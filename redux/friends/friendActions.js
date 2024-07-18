b422b199546382881ea8c719f6c982952d693d98
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-07-18 13:45:53 +0300

    update ui account page

--- redux/friends/friendActions.js
+++ redux/friends/friendActions.js
@@ -1,17 +1,47 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { authInstance } from "../../services/api.service";
+import { fetchAllUsers } from "../auth/authActions";
+
+// export const fetchPendingFriendRequests = createAsyncThunk(
+//   "friends/fetchPendingFriendRequests",
+//   async (_, { rejectWithValue }) => {
+//     try {
+//       const response = await authInstance.get("unaccepted-friendship-requests");
+//       return response.data;
+//     } catch (err) {
+//       return rejectWithValue(err.response.data);
+//     }
+//   }
+// );
 
 export const fetchPendingFriendRequests = createAsyncThunk(
   "friends/fetchPendingFriendRequests",
-  async (_, { rejectWithValue }) => {
+  async (_, { rejectWithValue, getState, dispatch }) => {
     try {
+      // Fetch pending friend requests
       const response = await authInstance.get("unaccepted-friendship-requests");
-      return response.data;
+      const pendingFriendRequests = response.data;
+
+      // Ensure all users are fetched
+      await dispatch(fetchAllUsers());
+      const allUsers = getState().auth.allUsers; // Adjust according to your store structure
+
+      // Enhance pending friend requests with user details
+      const enhancedRequests = pendingFriendRequests.map((request) => {
+        const userDetail = allUsers.find((user) => user.id === request.friend);
+        return {
+          ...request,
+          friendDetails: userDetail,
+        };
+      });
+
+      return enhancedRequests;
     } catch (err) {
       return rejectWithValue(err.response.data);
     }
   }
 );
+
 export const sendFriendRequest = createAsyncThunk(
   "friends/sendFriendRequest",
   async (data, { rejectWithValue }) => {
--- screens/account/index.js
+++ screens/account/index.js
@@ -1,44 +1,190 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Alert,
+  RefreshControl,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import { fetchUser } from "../../redux/auth/authActions";
-import { Text, Button } from "../../components";
+import {
+  fetchFriends,
+  fetchPendingFriendRequests,
+} from "../../redux/friends/friendActions";
+import { Text, Button, FriendRequest } from "../../components";
+import { ActivityIndicator, Avatar, Divider } from "react-native-paper";
 import { useSelector, useDispatch } from "react-redux";
+import { persistor } from "../../redux/store";
 
-export default function Account() {
+export default function Account({ navigation }) {
   const { user, loading, error } = useSelector((state) => state.auth);
+  const {
+    friends,
+    pendingRequests,
+    sentFriendRequest,
+    error: friendError,
+  } = useSelector((state) => state.friends);
 
   const dispatch = useDispatch();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const firstName = user?.["first_name"] || "";
+  const lastName = user?.["last_name"] || "";
+  const initials = (firstName[0] || "") + (lastName[0] || "");
 
   useEffect(() => {
-    dispatch(fetchUser());
-  }, []);
+    const unsubscribe = navigation.addListener("focus", () => {
+      dispatch(fetchUser());
+      dispatch(fetchFriends());
+      dispatch(fetchPendingFriendRequests());
+    });
 
-  console.log(user);
+    return unsubscribe;
+  }, [navigation]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    dispatch(fetchUser());
+    dispatch(fetchFriends());
+    dispatch(fetchPendingFriendRequests()).finally(() => setRefreshing(false));
+  };
+
+  const handleLogout = () => {
+    persistor.purge();
+    console.info("You have been logged out");
+  };
+
+  const handleDeleteAccount = () => {
+    Alert.alert(
+      "Are you sure you want to delete your account?", // Title
+      "This action cannot be undone.", // Message
+      [
+        {
+          text: "Yes",
+          onPress: () => {
+            // dispatch(deleteAccount());
+            console.log("Account deleted");
+          },
+        },
+        {
+          text: "No",
+          onPress: () => {
+            console.log("Account not deleted");
+          },
+        },
+      ]
+    );
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="orange" />
+      </View>
+    );
+  }
+
+  const renderPendingRequest = ({ item }) => (
+    <FriendRequest key={item.id} data={item} />
+  );
+  const renderFriend = ({ item }) => (
+    <Text key={item.email} value={item.email} variant={"body"} />
+  );
 
   return (
-    <View style={styles.container}>
-      <Text value={`Email: ${user?.email}`} variant={"subtitle"} />
-      <Text value={`First Name: ${user?.first_name}`} variant={"subtitle"} />
-      <Text value={`Last Name: ${user?.last_name}`} variant={"subtitle"} />
-      <Button
-        label="Logout"
-        variant={"contained"}
-        onPress={() => dispatch(logout())}
-      />
-      <Button
-        label="Delete Account"
-        variant={"outlined"}
-        onPress={() => dispatch(logout())}
-      />
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <Avatar.Text size={100} label={initials} />
+      <View style={{ alignItems: "center", marginVertical: 10 }}>
+        <Text
+          value={` ${user?.email}`}
+          variant={"body"}
+          style={{ fontWeight: 700 }}
+        />
+        <Text
+          value={` ${user?.first_name} ${user?.last_name}`}
+          variant={"body"}
+          style={{ fontWeight: 700 }}
+        />
+        <Button
+          label="Edit Profile"
+          variant={"outlined"}
+          onPress={() => {}}
+          icon="pencil"
+          contentStyle={{ flexDirection: "row-reverse" }}
+        />
+      </View>
+      <View style={styles.column}>
+        <Text value={`Friends`} variant={"subtitle"} />
+        {pendingRequests && pendingRequests.length > 0 ? (
+          <FlatList
+            data={pendingRequests}
+            renderItem={renderPendingRequest}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        ) : (
+          <Text value={"You have no pending requests"} variant={"body"} />
+        )}
+        <Divider />
+        {friends && friends.length > 0 ? (
+          <FlatList
+            data={friends}
+            renderItem={renderFriend}
+            keyExtractor={(item) => item.email}
+          />
+        ) : (
+          <Text value={"You have no friends"} variant={"body"} />
+        )}
+      </View>
+      <View style={styles.button}>
+        <Button
+          label="Logout"
+          variant={"contained"}
+          onPress={handleLogout}
+          icon="logout"
+          contentStyle={{ flexDirection: "row-reverse" }}
+        />
+        <Button
+          label="Delete Account"
+          variant={"outlined"}
+          onPress={handleDeleteAccount}
+          icon="delete"
+          contentStyle={{ flexDirection: "row-reverse" }}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
+    alignItems: "center",
+    margin: 20,
+    padding: 20,
+  },
+  column: {
+    alignSelf: "flex-start",
+    backgroundColor: "#fafafa",
+    padding: 10,
+    borderRadius: 5,
+    elevation: 1,
+    height: "100%",
+    maxHeight: 300,
+    width: "100%",
+  },
+  button: {
+    marginTop: 20,
+    width: "100%",
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
