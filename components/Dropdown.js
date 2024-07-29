41d93cf6bbfac0cb83f62b2a5f2e8dfdba7a0beb
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-07-29 12:26:35 +0300

    update dynamic dropdown

---
+++ components/Dropdown.js
@@ -0,0 +1,64 @@
+import {
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Text from "./Text";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function Dropdown({
+  showAccordion,
+  setShowAccordion,
+  children,
+}) {
+  return (
+    <View style={{ flex: 1, height: 500 }}>
+      <ImageBackground
+        imageStyle={{ objectFit: "cover" }}
+        borderRadius={10}
+        style={styles.accordionOuter}
+        source={require("../assets/pattern-single.png")}
+      >
+        <TouchableOpacity
+          style={styles.accordionInner}
+          onPress={() => setShowAccordion(!showAccordion)}
+        >
+          <Text
+            value=" RahaFest March 2024 Recap"
+            variant="subtitle"
+            style={{ color: "#fff" }}
+          />
+          <MaterialCommunityIcons
+            name={showAccordion ? "chevron-up" : "chevron-down"}
+            size={24}
+            color="white"
+          />
+        </TouchableOpacity>
+      </ImageBackground>
+      {showAccordion && <View style={{ flex: 1 }}>{children}</View>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  accordionOuter: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.2)",
+  },
+  accordionInner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // backgroundColor: "rgba(0, 0, 0, 1)",
+    padding: 20,
+    borderRadius: 10,
+    width: "100%",
+  },
+});
--- screens/club/landing/Gallery.js
+++ screens/club/landing/Gallery.js
@@ -1,12 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Image,
-  Modal,
-  TouchableOpacity,
-  ImageBackground,
-} from "react-native";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import { StyleSheet, View, Image, Modal, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import MasonryList from "react-native-masonry-list";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,39 +7,48 @@ import { fetchGallery } from "../../../redux/news/newsActions";
 import { ActivityIndicator } from "react-native-paper";
 import { rahaImageApi } from "../../../services/api.service";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Text } from "../../../components";
+import { Text, Dropdown } from "../../../components";
 
 export default function Media() {
   const dispatch = useDispatch();
   const { gallery, loading } = useSelector((state) => state.news);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
-  const [images, setImages] = useState([]);
   const [showAccordion, setShowAccordion] = useState(false);
-
-  // console.log(gallery);
 
   useEffect(() => {
     dispatch(fetchGallery());
   }, [dispatch]);
 
-  useEffect(() => {
+  const formattedImages = useMemo(() => {
     if (gallery) {
-      const formattedImages = gallery.map((item) => {
-        const uri = rahaImageApi + item.image;
-        return {
-          uri,
-          id: item.id.toString(),
-        };
-      });
-      setImages(formattedImages);
+      return gallery.map((item) => ({
+        uri: rahaImageApi + item.image,
+        id: item.id.toString(),
+      }));
     }
+    return [];
   }, [gallery]);
 
-  const onRefresh = () => {
+  const onRefresh = useCallback(() => {
     dispatch(fetchGallery());
-  };
+  }, [dispatch]);
+
+  const renderImage = useCallback(
+    ({ uri, id }) => (
+      <View style={styles.imageContainer} key={id}>
+        <Image
+          source={{ uri }}
+          style={styles.image}
+          resizeMode="cover"
+          onError={(e) => {
+            console.error("Image failed to load", e.nativeEvent.error);
+          }}
+        />
+      </View>
+    ),
+    []
+  );
 
   if (loading) {
     return (
@@ -56,18 +58,7 @@ export default function Media() {
     );
   }
 
-  const renderImage = ({ uri, id }) => (
-    <View style={styles.imageContainer} key={id}>
-      <Image
-        source={{ uri }}
-        style={styles.image}
-        resizeMode="cover"
-        onError={(e) => {
-          console.error("Image failed to load", e.nativeEvent.error);
-        }}
-      />
-    </View>
-  );
+  console.log("Gallery", showAccordion);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -76,50 +67,13 @@ export default function Media() {
         variant="subtitle"
         style={{ color: "#fff", alignSelf: "center", margin: 10, padding: 10 }}
       />
-      {/* <ImageBackground
-        style={styles.accordionOuter}
-        source={require("../../../assets/pattern-single.png")}
+      <Dropdown
+        showAccordion={showAccordion}
+        setShowAccordion={setShowAccordion}
       >
-        <TouchableOpacity
-          style={styles.accordionInner}
-          onPress={() => setShowAccordion(!showAccordion)}
-        >
-          <Text
-            value="What to expect at the upcoming Raha Rave 2024"
-            variant="subtitle"
-            style={{ color: "#fff" }}
-          />
-          <MaterialCommunityIcons
-            name={showAccordion ? "chevron-up" : "chevron-down"}
-            size={24}
-            color="white"
-          />
-        </TouchableOpacity>
-      </ImageBackground> */}
-      <ImageBackground
-        style={styles.accordionOuter}
-        source={require("../../../assets/pattern-single.png")}
-      >
-        <TouchableOpacity
-          style={styles.accordionInner}
-          onPress={() => setShowAccordion(!showAccordion)}
-        >
-          <Text
-            value=" RahaFest March 2024 Recap"
-            variant="subtitle"
-            style={{ color: "#fff" }}
-          />
-          <MaterialCommunityIcons
-            name={showAccordion ? "chevron-up" : "chevron-down"}
-            size={24}
-            color="white"
-          />
-        </TouchableOpacity>
-      </ImageBackground>
-      {showAccordion && (
-        <View style={{ flex: 1 }}>
+        {showAccordion && (
           <MasonryList
-            images={images.map((img) => ({
+            images={formattedImages.map((img) => ({
               uri: img.uri,
               id: img.id,
               component: renderImage({ uri: img.uri, id: img.id }), // Custom render function
@@ -135,25 +89,25 @@ export default function Media() {
             onRefresh={onRefresh}
             refreshing={loading}
           />
-          <Modal
-            visible={modalVisible}
-            transparent={true}
-            onRequestClose={() => setModalVisible(false)}
-          >
-            <View style={styles.modalBackground}>
-              <TouchableOpacity
-                style={styles.fullscreenImageContainer}
-                onPress={() => setModalVisible(false)}
-              >
-                <Image
-                  source={{ uri: selectedImage }}
-                  style={styles.fullscreenImage}
-                />
-              </TouchableOpacity>
-            </View>
-          </Modal>
-        </View>
-      )}
+        )}
+        <Modal
+          visible={modalVisible}
+          transparent={true}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalBackground}>
+            <TouchableOpacity
+              style={styles.fullscreenImageContainer}
+              onPress={() => setModalVisible(false)}
+            >
+              <Image
+                source={{ uri: selectedImage }}
+                style={styles.fullscreenImage}
+              />
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </Dropdown>
       <StatusBar style="light" />
     </SafeAreaView>
   );
@@ -176,24 +130,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  accordionOuter: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    margin: 10,
-    objectFit: "cover",
-    backgroundRepeat: "repeat",
-  },
-  accordionInner: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 1)",
-    padding: 20,
-    borderRadius: 10,
-    width: "100%",
   },
   modalBackground: {
     flex: 1,
