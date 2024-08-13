b35c9448a4aa2cebcc766895b756bc47719bf874
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-08-13 16:05:09 +0300

    refactor events

--- components/VideoPlayer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-import YoutubePlayer from "react-native-youtube-iframe";
-
-export default VideoPlayer = ({ video }) => {
-  return <YoutubePlayer height={200} play={false} videoId={video} />;
-};
