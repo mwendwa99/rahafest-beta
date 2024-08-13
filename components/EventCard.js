38bb7c702c5c8ad67e0efa642d90b2d1f36b8e9f
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-08-13 16:36:57 +0300

    update event page

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,37 +1,71 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import Text from "./Text";
 
 const EventCard = ({ deal }) => {
+  const { user, token } = useSelector((state) => state.auth);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const {
-    image,
-    name,
+    banner,
+    title,
     time,
-    location,
-    ticketType,
+    description,
     discount,
     oldPrice,
     newPrice,
+    expired,
+    tags,
   } = deal;
+
+  useEffect(() => {
+    if (user && token) {
+      setIsAuthenticated(true);
+    }
+  }, [user, token]);
+
+  console.log(JSON.stringify(deal));
 
   return (
     <View style={styles.container}>
       {/* Event Banner */}
-      <Image source={image} style={styles.banner} />
+      <Image source={{ uri: banner }} style={styles.banner} />
 
       {/* Event Details */}
       <View style={styles.detailsContainer}>
-        <Text style={styles.eventName}>{name}</Text>
-        <Text style={styles.eventTime}>{time}</Text>
-        <Text style={styles.eventLocation}>{location}</Text>
-        <Text style={styles.ticketType}>{ticketType}</Text>
+        <Text value={title} style={styles.eventName} variant="subtitle" />
+        <FlatList
+          data={tags}
+          renderItem={({ item }) => (
+            <View style={styles.chip}>
+              <Text value={`${item.tag}`} variant={"small"} />
+            </View>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+        <Text value={description} style={styles.eventTime} variant="body" />
       </View>
 
       {/* Discount and New Price */}
-      <View style={styles.priceContainer}>
-        <Text style={styles.discount}>{discount}% OFF</Text>
-        <Text style={styles.oldPrice}>KES {oldPrice}</Text>
-        <Text style={styles.newPrice}>KES {newPrice}</Text>
-      </View>
+      {expired ? (
+        <View style={styles.priceContainer}>
+          <Text
+            variant={"subtitle"}
+            style={{ color: "red" }}
+            value={"Expired"}
+          />
+          <Text style={styles.discount}>Expired</Text>
+        </View>
+      ) : (
+        <View style={styles.priceContainer}>
+          <Text style={styles.discount}>{discount}% OFF</Text>
+          <Text style={styles.oldPrice}>KES {oldPrice}</Text>
+          <Text style={styles.newPrice}>KES {newPrice}</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -67,17 +101,18 @@ const styles = StyleSheet.create({
     fontSize: 13,
     color: "#1b1b1b",
   },
-  ticketType: {
-    fontSize: 12,
-    color: "#C0C0C0",
+  chip: {
+    borderWidth: 1,
+    borderColor: "#666",
+    borderRadius: 20,
+    padding: 10,
+    marginRight: 4,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
   },
   priceContainer: {
     alignItems: "flex-end",
-  },
-  discount: {
-    fontSize: 14,
-    color: "red",
-    fontWeight: "bold",
   },
   oldPrice: {
     fontSize: 12,
--- screens/events/Events.js
+++ screens/events/Events.js
@@ -1,19 +1,55 @@
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, RefreshControl, View } from "react-native";
 import { EventCard } from "../../components";
 import { DealData } from "../../data";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchEvents } from "../../redux/events/eventActions";
+import { ActivityIndicator } from "react-native-paper";
 
 export default function Events() {
+  const dispatch = useDispatch();
+  const { events, loading, error } = useSelector((state) => state.events);
+
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  // console.log(events);
+
+  const handleRefresh = () => {
+    dispatch(fetchEvents());
+  };
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator animating={true} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>{error}</Text>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DealData}
+        data={events}
         renderItem={({ item }) => <EventCard deal={item} />}
         keyExtractor={(item) => item.id}
         numColumns={1}
         contentContainerStyle={styles.contentContainer}
         showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={handleRefresh} />
+        }
       />
       <StatusBar style="light" />
     </SafeAreaView>
