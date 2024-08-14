9474979337a36e0e3da9efcc02d829fc09479e83
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-08-14 12:56:34 +0300

    update event

--- screens/events/Events.js
+++ screens/events/Events.js
@@ -1,22 +1,53 @@
-import { StyleSheet, FlatList, RefreshControl, View } from "react-native";
+import { StyleSheet, FlatList, RefreshControl, View, Text } from "react-native";
 import { EventCard } from "../../components";
-import { DealData } from "../../data";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchEvents } from "../../redux/events/eventActions";
+import { useEffect, useState } from "react";
+import { fetchEvents, fetchTicketTypes } from "../../redux/events/eventActions";
 import { ActivityIndicator } from "react-native-paper";
 
 export default function Events() {
   const dispatch = useDispatch();
   const { events, loading, error } = useSelector((state) => state.events);
+  const { user, token } = useSelector((state) => state.auth);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [mergedEvents, setMergedEvents] = useState([]);
 
   useEffect(() => {
-    dispatch(fetchEvents());
+    const fetchData = async () => {
+      await dispatch(fetchEvents());
+    };
+    fetchData();
   }, [dispatch]);
 
-  // console.log(events);
+  useEffect(() => {
+    if (user && token) {
+      setIsAuthenticated(true);
+    }
+  }, [user, token]);
+
+  useEffect(() => {
+    const fetchAndMergeData = async () => {
+      if (events.length > 0) {
+        const updatedEvents = await Promise.all(
+          events.map(async (event) => {
+            const ticketTypesForEvent = await dispatch(
+              fetchTicketTypes(event.id)
+            );
+            return {
+              ...event,
+              ticketTypes: ticketTypesForEvent.payload || [],
+            };
+          })
+        );
+        setMergedEvents(updatedEvents);
+      }
+    };
+
+    fetchAndMergeData();
+  }, [events, dispatch]);
 
   const handleRefresh = () => {
     dispatch(fetchEvents());
@@ -41,8 +72,14 @@ export default function Events() {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={events}
-        renderItem={({ item }) => <EventCard deal={item} />}
+        data={mergedEvents}
+        renderItem={({ item }) => (
+          <EventCard
+            event={item}
+            ticketTypes={item.ticketTypes}
+            isAuthenticated={isAuthenticated}
+          />
+        )}
         keyExtractor={(item) => item.id}
         numColumns={1}
         contentContainerStyle={styles.contentContainer}
