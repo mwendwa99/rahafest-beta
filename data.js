e9594b6427e1eff75bdd1edcff7b2e754a8fdca0
QuincyKibuiya <quincyk45@gmail.com>
2024-08-27 11:16:59 +0300

    update merchandise card

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
 export const products = [
   {
     id: "1",
-    name: "Raha T-shirt",
+    name: "White Raha T-shirt",
     price: "1,000",
     image: require("./assets/test-merch.png"),
   },
   {
     id: "2",
-    name: "Raha Polo Shirt",
-    price: "1,500",
+    name: "Black Raha T-shirt",
+    price: "1,200",
     image: require("./assets/test-merch-2.png"),
   },
   {
     id: "3",
-    name: "Raha Hoodie",
+    name: "Black Raha Trouser",
     price: "3,000",
     image: require("./assets/test-merch-3.png"),
   },
   {
     id: "4",
-    name: "Raha T-shirt",
-    price: "1,000",
-    image: require("./assets/test-merch.png"),
+    name: "Black Raha Office Pants",
+    price: "2,500",
+    image: require("./assets/test-merch-4.png"),
   },
   {
     id: "5",
-    name: "Raha Polo Shirt",
+    name: "Grey Raha Sweatpants",
     price: "1,500",
-    image: require("./assets/test-merch-2.png"),
+    image: require("./assets/test-merch-5.png"),
   },
   {
     id: "6",
-    name: "Raha Hoodie",
-    price: "3,000",
-    image: require("./assets/test-merch-3.png"),
+    name: "Yellow Raha T-shirt",
+    price: "1,000",
+    image: require("./assets/test-merch-6.png"),
   },
 ];
 
