e511fabdcbdad3c15eb8ea81b9d29fe44330287f
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-10-15 12:18:36 +0300

    fix image render issues

--- components/AdCarousel.js
+++ components/AdCarousel.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Dimensions, Image, Linking, Text, View } from "react-native";
+import { Dimensions, Image, Linking, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Carousel from "react-native-reanimated-carousel";
 
@@ -17,6 +17,11 @@ const styles = {
   itemContainer: {
     flex: 1,
     justifyContent: "center",
+  },
+  image: {
+    width: "100%", // Full width of the carousel item
+    height: 200, // Set height for the image
+    resizeMode: "contain", // Ensure the image covers the area
   },
 };
 
@@ -37,15 +42,16 @@ function AdCarousel({ data }) {
         data={data}
         style={styles.carousel}
         scrollAnimationDuration={1000}
-        // onSnapToItem={(index) => console.log("current indxex:", index)}
         renderItem={({ item }) => {
           if (!item.is_active) return null; // Early return for inactive items
 
           return (
             <View style={styles.itemContainer}>
               <TouchableOpacity onPress={() => handlePress(item.url)}>
-                {/* <Text>{item.image}</Text> */}
-                <Image source={item.image} style={{ resizeMode: "cover" }} />
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.image} // Add the defined style here
+                />
               </TouchableOpacity>
             </View>
           );
