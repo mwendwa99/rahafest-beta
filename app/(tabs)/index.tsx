de5c0d3c2e21529781ff150a8d2f672a085578a8
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2024-11-27 11:55:45 +0300

    setup basic nav

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx woza edit this screen.</Text>
+      <Link href="/about" style={styles.button}>
+        About
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+  },
+});
